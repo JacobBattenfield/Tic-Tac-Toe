39da09b60a726c14a2c52184484abf796ecf422a
jaybat-code <jacobbattenfielddev@gmail.com>
2024-11-30 07:25:25 -0600

    Getting there....

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-const gameBoard = document.getElementById("container");
+const stamps = ['X','O'];
+let flag = 0;
+function CreateSections(){
+    for(let i = 0;i<9;i++){
+        const button = document.createElement("button");
+        button.setAttribute("class","btn");
+        button.setAttribute("id","button"+(i+1));
+        button.textContent = "";
+        const gameBoard = document.getElementById("container");
+        gameBoard.appendChild(button);
+    }
+}
+CreateSections();
+const btns = document.querySelectorAll(".btn");
+btns.forEach(btn => {
+    btn.addEventListener("click",()=>{
+        if(btn.innerHTML===""){
+            btn.innerHTML=stamps[flag];
+            flag=1-flag;
+            CheckWinner(btns);
+        }
+    })
+});
+function CheckWinner(buttons){
+    const winConditions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    for(let i = 0;i<winConditions.length;i++){
 
-gameBoard.innerText="Hell"
+    }
+}
+
